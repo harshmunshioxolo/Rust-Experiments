130ca72669905b21872034ba6e491af0801dd029
Harsh Munshi <h.munshi@oxolo.com>
2021-03-06 23:16:59 +0100

    strings

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod print;
 mod var;
 mod primitives;
+mod strings;
 
 fn main() {
     // print::run();
     //var::run();
-    primitives::run();
+    // primitives::run();
+    strings::run();
 }
